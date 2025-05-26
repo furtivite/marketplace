8ad91e86a1c90fa6d8b56ec6bf3cfb5f93784d7f
Egor Levchenko <antytoto@gmail.com>
2025-05-26 14:08:11 +0300

    feat: init tailwind v3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
-export const App: React.FC = () => {
-  return <div>Home</div>;
+const App: React.FC = () => {
+  return <h1 className="text-3xl font-bold text-blue-600">Tailwind работает</h1>;
 };
+
+export { App };
