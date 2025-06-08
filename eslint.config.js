5eeb5fe1f9c5996b6521e6a7a1274485eb04d0ad
Egor Levchenko <antytoto@gmail.com>
2025-06-08 11:15:56 +0300

    chore(lint): merge Airbnb & TS & React rules into flat ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,24 +5,39 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+    { ignores: ['dist'] },
+    {
+        extends: [
+            js.configs.recommended,
+            'airbnb',
+            'airbnb/hooks',
+            'airbnb-typescript',
+            ...tseslint.configs.recommended,
+            'plugin:storybook/recommended',
+        ],
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+        },
+        rules: {
+            // Основные правила форматирования по Airbnb
+            'indent': ['error', 2, { SwitchCase: 1 }],
+            'semi': ['error', 'always'],
+            'comma-dangle': ['error', 'always-multiline'],
+            'react/react-in-jsx-scope': 'off',
+            'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+            // Правила React Hooks
+            ...reactHooks.configs.recommended.rules,
+            // Правило storybook-refresh
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+        },
+    }
 )
