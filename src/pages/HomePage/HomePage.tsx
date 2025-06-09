80fdc1d51dddc6b934c5ae04506ad25913239ce3
Egor Levchenko <antytoto@gmail.com>
2025-06-09 16:03:34 +0300

    feat(homepage): scaffold HomePage with Layout wrapper

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,21 @@
+// src/pages/HomePage/HomePage.tsx
+import React from 'react';
+import { Layout } from '../../widgets/Layout/Layout';
+// import { HeroSection } from './ui/HeroSection'
+// import { BenefitsSection } from './ui/BenefitsSection'
+// import { BestSellingSection } from './ui/BestSellingSection'
+// import { BrowseCategorySection } from './ui/BrowseCategorySection'
+// import { FeaturedTabsSection } from './ui/FeaturedTabsSection'
+
+export const HomePage: React.FC = () => (
+  <Layout hasFooter hasNewsletter>
+    {/* <HeroSection /> */}
+    {/* <BenefitsSection /> */}
+    {/* <BestSellingSection /> */}
+    {/* <BrowseCategorySection /> */}
+    {/* <FeaturedTabsSection /> */}
+    Это главная страница
+  </Layout>
+);
+
+export default HomePage;
