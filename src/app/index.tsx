d984b330d91ee855d932333603c2b832921df4e9
Egor Levchenko <antytoto@gmail.com>
2025-06-20 12:07:23 +0300

    feat: connect StoreProvider and extend ESLint config for TypeScript imports

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,10 +6,13 @@ import '@fontsource/inter/500.css';
 import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import '@fontsource/manrope/800.css';
+import { StoreProvider } from '@/app/providers/StoreProvider/StoreProvider';
 import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
   </StrictMode>,
 );
